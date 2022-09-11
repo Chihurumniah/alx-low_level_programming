a081ed1b6f9e2f533e5b2608ab514e510698aaf5
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-09-11 07:26:37 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the programe
+ *
+ * Return: Return 0 if successfull
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
