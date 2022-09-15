317ca7c6b34a256e33a76ff42dce7735b564d626
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-09-15 07:37:53 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase
+ * @c: Function parameter
+ *
+ * Return: Return 1 or 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
