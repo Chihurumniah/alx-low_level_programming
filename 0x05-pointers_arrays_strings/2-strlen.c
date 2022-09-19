3c2789e0fd1a91cd889080ade08091f2c04a2d17
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-09-19 05:34:18 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strlen - Returns the length of a string
+ * @s: holds the string.
+ *
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+		return (strlen(s));
+}
