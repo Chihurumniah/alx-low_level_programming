b314faf2b31fb13abd88559b549b2929425342f1
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-09-21 11:58:22 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: char to check
+ * @src: char to check
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
