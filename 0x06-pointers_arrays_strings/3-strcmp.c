89aede1fe7422c11a75da4c51140a405cb11a879
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-09-22 04:14:09 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strcmp - Compares two string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Returns an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
