afd03dcb33d68ec02dd995a7d015312e4c162fb7
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-10-10 06:06:05 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _DOG_H
+#define _DOG_H
 
+/**
+ * struct dog - a new type
+ * @name: the name of the dog
+ * @age: the age of the dog (3.5)
+ * @owner: the name of the owner (Jay)
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
-#endif /* MAIN_H */
+#endif /* DOG */
