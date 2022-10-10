b77a3487db9371803c5a4633494159672e1e19b1
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-10-10 09:06:14 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog
- * @name: pointer to a variable to copy
- * @owner: pointer to a variable to copy
- * @age: float variable type
- *
- * Return: to dog_t or NULL if it fail
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* dog_t is the new name of dog */
-	/* two variables to know the size of the variables I need to copy */
-	int namerunner, ownerrunner;
-	/* allocate the *new_dog function and test if is null or not */
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-	if (new_dog == NULL)
-		return (new_dog);
-	/* time to know the size of the *name and *owner */
-	for (namerunner = 0; name[namerunner]; namerunner++)
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
 	{
-	}
-	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
-	{
-	}
-	/* Add in new_dog the size of the variables name and owner */
-	/* dereference new_dog to each of the variables */
-	/* plus 1 to the runners because I need to save space for \0 too */
-	new_dog->name = malloc((namerunner + 1) * sizeof(char));
-	new_dog->name = malloc((namerunner + 1) * sizeof(char));
-	/*check allocation was right if NULL free all the allocated memory*/
-	/* we freed memory from what is inside to the big function new_dog */
-	if (new_dog->name == NULL || new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
+		free(p_dog);
 		return (NULL);
 	}
-	/* once I have the memory allocated */
-	/* I start copying the variables to the malloc pointer *new_dog */
-	for (namerunner = 0; name[namerunner]; namerunner++)
-		new_dog->name[namerunner] = name[namerunner];
-	for (ownerrunner = 0; owner[ownerrunner]; ownerrunner++)
-		new_dog->owner[ownerrunner] = owner[ownerrunner];
-	/* now I set them to zero so the function return the output I need */
-	new_dog->name[namerunner] = 0;
-	new_dog->owner[ownerrunner] = 0;
-	new_dog->age = age;
-	return (new_dog);
+	for (lname = 0; name[lname]; lname++)
+		;
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+	p_dog->age = age;
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+	return (p_dog);
 }
