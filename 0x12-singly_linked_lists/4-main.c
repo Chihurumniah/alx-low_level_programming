14ca77b3ab90bfef89119aa4cec7b502d3ed2ae9
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-10-20 15:04:07 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -13,10 +13,13 @@ int main(void)
 	list_t *head;
 
 	head = NULL;
-	add_node_end(&head, "Anne");
-	add_node_end(&head, "Colton");
-	add_node_end(&head, "Corbin");
-	add_node_end(&head, "Daniel");
+	add_node_end(&head, "Bob");
+	add_node_end(&head, "&");
+	add_node_end(&head, "Kris");
+	add_node_end(&head, "love");
+	add_node_end(&head, "asm");
 	print_list(head);
+	free_list(head);
+	head = NULL;
 	return (0);
 }
