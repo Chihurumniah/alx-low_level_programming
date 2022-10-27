8c190e0c49e0f506a7dac51622aff130a9fd1635
Miracle Okonkwo <vvcahattapp@gmail.com>
2022-10-27 04:13:46 -0700

    task 0. 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stddef.h>
+
+int _putchar(char c);
+int check_valid_string(const char *b);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
+
+
